de1020969880f211d166b1cc696309fec5a8623a
lxy <492861128@qq.com>
2019-02-13 17:32:39 +0800

    add basic logic

--- app.js
+++ app.js
@@ -6,24 +6,11 @@ const port = 8080;
 
 http
   .createServer((require, response) => {
-    console.log("in server...");
-    response.writeHead(200, { "Content-Type": "text/plain" });
-    response.end("Hello World\n");
     require.on("data", data => {
       const buffer = Buffer.from(data);
       const jsonobj = JSON.parse(buffer.toString());
       const path = projectPath.get(jsonobj.project_id);
-      console.log(jsonobj);
       if (path) {
-        console.log(path);
-      }
-      console.log(
-        jsonobj.project_id,
-        projectPath,
-        jsonobj.project_id,
-        jsonobj["project_id"]
-      );
-      if (0) {
         const res1 = shell.cd("/home/bmap_doc/");
         console.log("res1", res1);
         // 执行git pull
@@ -32,9 +19,18 @@ http
         // 删除编译文件
         shell.exec("rm -rf ./build");
         // 执行编译
-        shell.exec("npm run build");
+        const buildresult = shell.exec("npm run build");
+        console.log("-------------buildresult:--------------");
+        console.log(buildresult);
+        console.log("-----------------------------------------");
+      } else {
+        console.error(
+          `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
+        );
       }
     });
+    response.writeHead(200, { "Content-Type": "text/plain" });
+    response.end("success");
   })
   .listen(port)
   .on("error", error => {
