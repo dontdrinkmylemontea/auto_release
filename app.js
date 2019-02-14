5a26d2f648e22b89ae1e42a75ff540f78386358d
lxy <492861128@qq.com>
2019-02-14 16:06:07 +0800

    删除log

--- app.js
+++ app.js
@@ -7,22 +7,12 @@ const port = 8080;
 http
   .createServer((require, response) => {
     let requestConfig = undefined;
-    console.log("in server");
     require.on("data", data => {
-      console.log("get data");
       const buffer = Buffer.from(data);
       const jsonobj = JSON.parse(buffer.toString());
       const config = projectPath.get(jsonobj.project_id);
       const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
       if (config && ref === config.releaseBranch) {
-        // setImmediate(() => {
-        //   shell.cd(config.path);
-        //   // 执行git pull
-        //   shell.exec(`git pull origin ${ref}`);
-        //   // 执行编译
-        //   const result = shell.exec(config.buildScript);
-        //   console.log("in immediate, result is", result);
-        // });
         requestConfig = config;
       } else {
         console.error(
@@ -31,14 +21,12 @@ http
       }
     });
     response.on("finish", () => {
-      console.log("finish sending response");
       if (requestConfig) {
         shell.cd(requestConfig.path);
         // 执行git pull
         shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
         // 执行编译
-        const result = shell.exec(requestConfig.buildScript);
-        console.log("in immediate, result is", result);
+        shell.exec(requestConfig.buildScript);
       }
     });
     response.writeHead(200, { "Content-Type": "text/plain" });
@@ -47,7 +35,6 @@ http
   .listen(port)
   .on("error", error => {
     console.error("error", error.message);
-  })
-  .on("");
+  });
 
 console.log(`server running on http://localhost:${port}`);
