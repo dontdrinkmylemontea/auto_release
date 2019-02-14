8c307e883ff6ae5ca0870192f66ac2ae603af768
lxy <492861128@qq.com>
2019-02-14 15:52:07 +0800

    脚本事件从request.on('data')改为response.on('finish')

--- app.js
+++ app.js
@@ -6,24 +6,36 @@ const port = 8080;
 
 http
   .createServer((require, response) => {
+    let requestConfig = undefined;
     require.on("data", data => {
       const buffer = Buffer.from(data);
       const jsonobj = JSON.parse(buffer.toString());
       const config = projectPath.get(jsonobj.project_id);
       const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
       if (config && ref === config.releaseBranch) {
-        setImmediate(() => {
-          shell.cd(config.path);
-          // 执行git pull
-          shell.exec(`git pull origin ${ref}`);
-          // 执行编译
-          const result = shell.exec(config.buildScript);
-          console.log("in immediate, result is", result);
-        });
+        // setImmediate(() => {
+        //   shell.cd(config.path);
+        //   // 执行git pull
+        //   shell.exec(`git pull origin ${ref}`);
+        //   // 执行编译
+        //   const result = shell.exec(config.buildScript);
+        //   console.log("in immediate, result is", result);
+        // });
+        requestConfig = config;
       } else {
         console.error(
           `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
         );
+      }
+    });
+    response.on("finish", () => {
+      if (requestConfig) {
+        shell.cd(requestConfig.path);
+        // 执行git pull
+        shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
+        // 执行编译
+        const result = shell.exec(requestConfig.buildScript);
+        console.log("in immediate, result is", result);
       }
     });
     response.writeHead(200, { "Content-Type": "text/plain" });
@@ -32,6 +44,7 @@ http
   .listen(port)
   .on("error", error => {
     console.error("error", error.message);
-  });
+  })
+  .on("");
 
 console.log(`server running on http://localhost:${port}`);
