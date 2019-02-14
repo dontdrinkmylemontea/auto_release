f5bce89f8342402475ee7983791898267b399b9e
lxy <492861128@qq.com>
2019-02-14 15:09:43 +0800

    先返回200再执行脚本

--- app.js
+++ app.js
@@ -12,12 +12,14 @@ http
       const config = projectPath.get(jsonobj.project_id);
       const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
       if (config && ref === config.releaseBranch) {
-        shell.cd(config.path);
-        // 执行git pull
-        shell.exec(`git pull origin ${ref}`);
-        // 执行编译
-        const result = shell.exec(config.buildScript);
-        console.log(result);
+        setImmediate(() => {
+          shell.cd(config.path);
+          // 执行git pull
+          shell.exec(`git pull origin ${ref}`);
+          // 执行编译
+          const result = shell.exec(config.buildScript);
+          console.log(result);
+        });
       } else {
         console.error(
           `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
