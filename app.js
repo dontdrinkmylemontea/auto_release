04cb9371d3ac210f82267841460772fee8c7b367
dontdrinkmylemmontea <492861128@qq.com>
2019-11-22 16:52:06 +0800

    应该好了吧？

--- app.js
+++ app.js
@@ -4,37 +4,38 @@ var { projectPath, listenPort } = require("./config.js");
 
 http
   .createServer((request, response) => {
-    if (!request.body) {
-      console.log(request.body);
-      response.writeHead(400, { "Content-Type": "text/plain" });
-      response.end("请输入项目名称");
-      return;
-    }
-    let requestConfig = undefined;
-    const jsonobj = JSON.parse(request.body);
-    const config = projectPath.get(jsonobj.repository.name);
-    const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
-    if (config && ref === config.releaseBranch) {
-      requestConfig = config;
-    } else {
-      console.error(
-        `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
-      );
-    }
-    if (requestConfig) {
-      shell.cd(requestConfig.path);
-      // 执行git pull
-      shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
-      // 执行编译
-      shell.exec(requestConfig.buildScript);
-      // 执行部署
-      shell.exec(requestConfig.publishScript);
-      response.writeHead(200, { "Content-Type": "text/plain" });
-      response.end("success");
-    } else {
-      response.writeHead(404, { "Content-Type": "text/plain" });
-      response.end("没找到该项目的配置");
-    }
+    let data = "";
+    request.on("data", buf => {
+      data += buf;
+    });
+
+    request.on("end", () => {
+      let requestConfig = undefined;
+      const jsonobj = JSON.parse(Buffer.from(data).toString());
+      const config = projectPath.get(jsonobj.repository.name);
+      const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
+      if (config && ref === config.releaseBranch) {
+        requestConfig = config;
+      } else {
+        console.error(
+          `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
+        );
+      }
+      if (requestConfig) {
+        shell.cd(requestConfig.path);
+        // 执行git pull
+        shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
+        // 执行编译
+        shell.exec(requestConfig.buildScript);
+        // 执行部署
+        shell.exec(requestConfig.publishScript);
+        response.writeHead(200, { "Content-Type": "text/plain" });
+        response.end("success");
+      } else {
+        response.writeHead(404, { "Content-Type": "text/plain" });
+        response.end("没找到该项目的配置");
+      }
+    });
   })
   .listen(listenPort)
   .on("error", error => {
