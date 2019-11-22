75007fffeefd15b7c85fc489e02b09885fc9fa1a
dontdrinkmylemmontea <492861128@qq.com>
2019-11-22 16:35:54 +0800

    修改了处理请求的方式

--- app.js
+++ app.js
@@ -3,44 +3,38 @@ var shell = require("shelljs");
 var { projectPath, listenPort } = require("./config.js");
 
 http
-  .createServer((require, response) => {
+  .createServer((request, response) => {
+    if (!request.body) {
+      console.log(request.body);
+      response.writeHead(400, { "Content-Type": "text/plain" });
+      response.end("请输入项目名称");
+      return;
+    }
     let requestConfig = undefined;
-    let databuf = "";
-    require.on("data", data => {
-      console.log("data = ", data);
-      databuf += data;
-    });
-    require.on("finish", () => {
-      const buffer = Buffer.from(databuf);
-      console.log("buffer = ", buffer);
-      const jsonobj = JSON.parse(buffer.toString());
-      console.log("----------------jsonobj--------------------------");
-      console.log(jsonobj);
-      console.log("------------------------------------------");
-      const config = projectPath.get(jsonobj.repository.name);
-      const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
-      if (config && ref === config.releaseBranch) {
-        requestConfig = config;
-      } else {
-        console.error(
-          `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
-        );
-      }
-      if (requestConfig) {
-        shell.cd(requestConfig.path);
-        // 执行git pull
-        shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
-        // 执行编译
-        shell.exec(requestConfig.buildScript);
-        // 执行部署
-        shell.exec(requestConfig.publishScript);
-        response.writeHead(200, { "Content-Type": "text/plain" });
-        response.end("success");
-      } else {
-        response.writeHead(404, { "Content-Type": "text/plain" });
-        response.end(`"没找到该项目的配置"`);
-      }
-    });
+    const jsonobj = JSON.parse(request.body);
+    const config = projectPath.get(jsonobj.repository.name);
+    const ref = jsonobj.ref ? jsonobj.ref.split("/")[2] : 0;
+    if (config && ref === config.releaseBranch) {
+      requestConfig = config;
+    } else {
+      console.error(
+        `error: project: ${jsonobj.repository.name} 没有配置该项目相关路径`
+      );
+    }
+    if (requestConfig) {
+      shell.cd(requestConfig.path);
+      // 执行git pull
+      shell.exec(`git pull origin ${requestConfig.releaseBranch}`);
+      // 执行编译
+      shell.exec(requestConfig.buildScript);
+      // 执行部署
+      shell.exec(requestConfig.publishScript);
+      response.writeHead(200, { "Content-Type": "text/plain" });
+      response.end("success");
+    } else {
+      response.writeHead(404, { "Content-Type": "text/plain" });
+      response.end("没找到该项目的配置");
+    }
   })
   .listen(listenPort)
   .on("error", error => {
